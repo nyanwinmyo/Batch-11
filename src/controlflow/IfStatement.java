140d467e5ff79eb3c372f961ac7a4165ca3786d0
Nyanwin Myo <nyanwinmyo@gmail.com>
2022-06-06 16:31:43 +0630

    part one lesson1

--- src/controlflow/IfStatement.java
+++ src/controlflow/IfStatement.java
@@ -2,8 +2,8 @@ package controlflow;
 
 public class IfStatement {
     public static void main(String[] args) {
-        int a=10;
-        if(a <15){
+        int a=15;
+        if(a <=15){
             System.out.println(a);
         }
     }
